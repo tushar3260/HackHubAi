07cdc198b02358e4654ea4b75dac7492012ba8d0
Tushar Bansal <tusharbansal3260@gmail.com>
2025-11-11 11:15:57 +0530

    setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from 'react';
+import CardNav from './components/CardNav';
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+   <>
+    <CardNav/>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
+      <h1 className="text-5xl font-bold mb-4">Tailwind + React ✅</h1>
+      <button className="px-6 py-2 bg-blue-500 rounded-lg hover:bg-blue-600 transition">
+        Click Me
+      </button>
+    </div></>
+  );
 }
-
-export default App
